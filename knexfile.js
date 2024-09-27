ca4dd5b2616334e0015069ec6978b1346415d304
Effie Fowler <effiejofowler@gmail.com>
2024-09-27 14:39:01 +0100

    update knexfile to match realtime database

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,14 @@
 import "dotenv/config";
 
-const { DB_LOCAL_DBNAME, DB_LOCAL_USER, DB_LOCAL_PASSWORD, DB_LOCAL_HOST } =
-  process.env;
+const { DB_DBNAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 export default {
   client: "mysql2",
   connection: {
-    host: DB_LOCAL_HOST,
-    user: DB_LOCAL_USER,
-    password: DB_LOCAL_PASSWORD,
-    database: DB_LOCAL_DBNAME,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DBNAME,
     charset: "utf8",
   },
 };
